a81d473431813c131e1f54bce9f8abd80e78fe61
RainBowBl00d <Innarraudsepp@gmail.com>
2026-03-10 16:33:00 +0200

    Initial commit

---
+++ restaurant-reservation-api/src/main/java/com/example/restaurantreservationapi/RestaurantReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationApiApplication.class, args);
+	}
+
+}
